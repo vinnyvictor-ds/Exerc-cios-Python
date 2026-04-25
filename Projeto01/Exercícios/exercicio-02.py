e51c4ad996bc36426b333910b5b767092a91328e
Vinny Victor Lima da Silva <vinny.victor@ufrpe.br>
2026-04-25 12:25:40 -0300

    Criei um programa que ler um número, averigua os divisores desse número através da estrutura de repetição 'for' e retorna se este número é primo ou não com base na própria definição de números primos.

---
+++ Projeto01/Exercícios/exercicio-02.py
@@ -0,0 +1,15 @@
+#Exercício 52: Crie um programa que leia um número inteiro e diga se ele é primo ou não.
+try:
+    verificador = 0
+    numero = int(input('Digite um número para verificarmos se ele é primo: '))
+    for c in range(1, numero + 1):
+        if numero % c == 0:
+            verificador = verificador + 1
+        else:
+            continue
+    if verificador == 2:
+        print(f'Há {verificador} divisores. Portanto, {numero} é primo.')
+    else:
+        print(f'Há {verificador} divisores. Portanto, {numero} não é primo.')
+except ValueError:
+    print('O valor digitado pelo usuário é inválido. Tente novamente!')
