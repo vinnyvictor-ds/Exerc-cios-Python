8b31e23db4a953a8fc42d9a1956a3bfa7ef3950d
Vinny Victor Lima da Silva <vinny.victor@ufrpe.br>
2026-04-26 08:54:02 -0300

    Exercício que varre lista e descobre pessoas maiores e menores de idade. Neste exercício usei list comprehension para agilizar o processo e deixá-lo o mais limpo possível.

--- Projeto01/Exercícios/exercicio-03.py
+++ Projeto01/Exercícios/exercicio-03.py
@@ -6,8 +6,8 @@ for contador in range(0,3):
     idade = int(input(f'Qual a idade de {nome}?'))
     dados_pessoa = [nome, idade]            #criei uma lista que guarda duas variáveis já definidas: nome, idade.
     pessoas.append(dados_pessoa)            #adicionei essa lista 'dados_pessoa' dentro da lista 'pessoas'.
-maiores_idade = [pessoa[1] for pessoa in pessoas if pessoa[1] >= 18] #Utilizei list comprehension para separar pessoas >= 18 anos
-menores_idade = [pessoa[1] for pessoa in pessoas if pessoa[1] < 18] #Utilizei list comprehension para separar pessoas < 18 anos
+maiores_idade = [pessoa for pessoa in pessoas if pessoa[1] >= 18] #Utilizei list comprehension para separar pessoas >= 18 anos
+menores_idade = [pessoa for pessoa in pessoas if pessoa[1] < 18] #Utilizei list comprehension para separar pessoas < 18 anos
 print(f'As pessoas que são maiores de idade são: {maiores_idade}')
 print(f'Idades maiores que 18 anos: {maiores_idade}')
 print(f'Idades menores que 18 anos: {menores_idade}')
